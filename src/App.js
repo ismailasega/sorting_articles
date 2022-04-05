3b7b5099d50d2fbee316c98dcded67bb6c6c2404
Asega Debele Ismail <asega03@gmail.com>
2022-04-05 09:29:27 +0300

    Most Upvoted and Most Recent Filters Added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import 'h8k-components';
 
@@ -6,17 +6,33 @@ import Articles from './components/Articles';
 
 const title = "Sorting Articles";
 
-function App({articles}) {
+function App({ articles }) {
+
+    // Declaring sorting by descending order
+    const sortByUpvotesDesc = (a,b) => b.upvotes - a.upvotes
+    const sortByDateDesc = (a, b) => b.date.localeCompare(a.date);
+    
+    // Declaring articles order state to keep truck and by default they are sorted by Upvotes
+    const [articlesOrder, setArticlesOrder] = useState('byUpvotesDesc')
+    
+    // Declaring most upvoted and most recent button click Sorting filters by descending order
+    const onMostUpvotedClick = () => {
+      setArticlesOrder('byUpvotesDesc')
+    }
+  
+    const onMostRecentClick = () => {
+      setArticlesOrder('byDatesDesc')
+    }
 
     return (
         <div className="App">
             <h8k-navbar header={title}></h8k-navbar>
             <div className="layout-row align-items-center justify-content-center my-20 navigation">
                 <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                <button data-testid="most-upvoted-link" className="small">Most Upvoted</button>
-                <button data-testid="most-recent-link" className="small">Most Recent</button>
+                <button data-testid="most-upvoted-link" className="small" onClick={onMostUpvotedClick}>Most Upvoted</button>
+                <button data-testid="most-recent-link" className="small" onClick={onMostRecentClick}>Most Recent</button>
             </div>
-            <Articles articles={articles}/>
+            <Articles articles={articles.sort(articlesOrder === 'byUpvotesDesc' ? sortByUpvotesDesc : sortByDateDesc)} />
         </div>
     );
 
